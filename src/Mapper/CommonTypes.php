63da7421a4dac580fb4663f85df95f40553f8862
Petr Besir Horáček <petr@mondayfactory.cz>
2021-11-12 13:54:01 +0100

    Extended common types

--- src/Mapper/CommonTypes.php
+++ src/Mapper/CommonTypes.php
@@ -12,28 +12,45 @@ class CommonTypes
 	public function __construct()
 	{
 		$this->knownTypes = [
-			'\\Ramsey\\Uuid\\UuidInterface' => [$this, 'ramseyUuid'],
-			'\\Ramsey\\Uuid\\Uuid' => [$this, 'ramseyUuid'],
+			'Ramsey\\Uuid\\UuidInterface' => [$this, 'ramseyUuid'],
+			'Ramsey\\Uuid\\Uuid' => [$this, 'ramseyUuid'],
+			'Consistence\\Enum\\Enum' => [$this, 'consistenceEnum'],
 		];
 	}
 
 	public function getMapper(string $className): ?MapperObjectInterface
 	{
 		if (strlen($className) === 0) {
-			return null;
-		}
-
-		if (! array_key_exists($className, $this->knownTypes)) {
 
 			return null;
 		}
 
-		return call_user_func($this->knownTypes[$className]);
+		if (!class_exists($className) && !interface_exists($className)) {
+
+			return null;
+		}
+
+		$classes = class_parents($className);
+		array_unshift($classes, $className);
+
+		foreach ($classes as $class) {
+			if (array_key_exists($class, $this->knownTypes)) {
+
+				return call_user_func($this->knownTypes[$class], $className);
+			}
+		}
+
+		return null;
 	}
 
 	private function ramseyUuid(): MapperObjectInterface
 	{
-		return new MethodTypesMapper('\\Ramsey\\Uuid\\Uuid' ,'\\Ramsey\\Uuid\\UuidInterface', 'fromString', 'toString');
+		return new MethodTypesMapper('Ramsey\\Uuid\\Uuid', 'Ramsey\\Uuid\\UuidInterface', 'fromString', 'toString');
+	}
+
+	private function consistenceEnum(string $className): MapperObjectInterface
+	{
+		return new MethodTypesMapper($className, null, 'get', 'getValue');
 	}
 
 }
